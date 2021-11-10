b7c26295d63c4850afd2071ee244cfc15bb97ec7
AndreyTrubitsyn <iverstoon@icloud.com>
2021-11-10 15:58:57 +0400

    more

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -323,20 +323,11 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  // const num = {
-  //   1: 'one',
-  //   2: 'two',
-  //   3: 'three',
-  //   4: 'four',
-  //   5: 'five',
-  //   6: 'six',
-  //   7: 'seven',
-  //   8: 'eight',
-  //   9: 'nine',
-  // };
-  // const newArr = arr.sort((a, b) => ;
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numStr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const changedArr = arr.map((item) => num[numStr.indexOf(item)]);
+  return changedArr.sort().map((item) => numStr[num.indexOf(item)]);
 }
 
 /**
